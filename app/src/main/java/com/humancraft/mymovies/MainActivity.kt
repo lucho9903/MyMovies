ca04b1d4d7fb5ec0e007bd2e18dd6b0b7e0c38aa
lucho9903 <luisfgn99@gmail.com>
2024-12-04 22:04:04 -0500

    se crea la primer pantalla de la aplicacion

--- app/src/main/java/com/humancraft/mymovies/MainActivity.kt
+++ app/src/main/java/com/humancraft/mymovies/MainActivity.kt
@@ -5,19 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.humancraft.mymovies.ui.theme.MyMoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,6 +36,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun MediaItem() {
+    Column {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(color = Color.Red)
+        ) {
+
+        }
+        Text(text = "Titulo 1")
+    }
+
+}
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -43,20 +61,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(
-    showBackground = true, widthDp = 400, heightDp = 200
-)
-@Composable
-fun GreetingPreview() {
-    MyMoviesTheme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly    //Organiza el contenido para que ocupe o no determinado espacio.
-
-        ) {
-            Greeting(name = "luis", modifier = Modifier.background(Color.LightGray).weight(3f))
-            Greeting(name = "fenando", modifier = Modifier.background(Color.Yellow).weight(2f))
-        }
-    }
-}
