e42e4ce50706fbdcc3b26965b00b8aa9213e8951
lucho9903 <luisfgn99@gmail.com>
2024-12-04 19:57:51 -0500

    se crea proyecto myMovies

---
+++ app/src/main/java/com/humancraft/mymovies/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.humancraft.mymovies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.humancraft.mymovies.ui.theme.MyMoviesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyMoviesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true,
+            widthDp = 400, heightDp = 200)
+@Composable
+fun GreetingPreview() {
+    MyMoviesTheme {
+        Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+        ){
+            Greeting(name = "luis", modifier = Modifier.align(Alignment.BottomEnd))
+            Greeting("fernando")
+
+        }
+    }
+}
